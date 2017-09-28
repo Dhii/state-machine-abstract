dfa5c219896dbc1345c99266eaa3ba293da94a63
Miguel Muscat <miguelmuscat93@gmail.com>
2017-09-28 13:39:35 +0200

    Added `StateMachineAwareTrait` and test

---
+++ src/StateMachineAwareTrait.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Dhii\State;
+
+use Dhii\Util\String\StringableInterface as Stringable;
+use Exception as RootException;
+use InvalidArgumentException;
+
+/**
+ * Common functionality for objects that are aware of state machine.
+ *
+ * @since [*next-version*]
+ */
+trait StateMachineAwareTrait
+{
+    /**
+     * The state machine instance.
+     *
+     * @since [*next-version*]
+     *
+     * @var StateMachineInterface|null
+     */
+    protected $stateMachine;
+
+    /**
+     * Retrieves the state machine associated with this instance.
+     *
+     * @since [*next-version*]
+     *
+     * @return StateMachineInterface|null The state machine instance, if any.
+     */
+    protected function _getStateMachine()
+    {
+        // todo
+    }
+
+    /**
+     * Sets the state machine for this instance.
+     *
+     * @since [*next-version*]
+     *
+     * @param StateMachineInterface|null $stateMachine The state machine instance or null
+     */
+    protected function _setStateMachine($stateMachine)
+    {
+        // todo
+    }
+
+    /**
+     * Creates a new invalid argument exception.
+     *
+     * @since [*next-version*]
+     *
+     * @param string|Stringable|null $message  The error message, if any.
+     * @param int|null               $code     The error code, if any.
+     * @param RootException|null     $previous The inner exception for chaining, if any.
+     * @param mixed|null             $argument The invalid argument, if any.
+     *
+     * @return InvalidArgumentException The new exception.
+     */
+    abstract protected function _createInvalidArgumentException(
+        $message = null,
+        $code = null,
+        RootException $previous = null,
+        $argument = null
+    );
+
+    /**
+     * Translates a string, and replaces placeholders.
+     *
+     * @since [*next-version*]
+     * @see   sprintf()
+     *
+     * @param string $string  The format string to translate.
+     * @param array  $args    Placeholder values to replace in the string.
+     * @param mixed  $context The context for translation.
+     *
+     * @return string The translated string.
+     */
+    abstract protected function __($string, $args = [], $context = null);
+}
